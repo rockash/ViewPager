621d68d28329439b0dfd5f1d1e596134907f2374
rockash <pillaiashwin@rocketmail.com>
2016-07-01 18:31:55 +0530

    login with prms web service

--- app/src/main/java/selfhelp/viewpager/LoginActivity.java
+++ app/src/main/java/selfhelp/viewpager/LoginActivity.java
@@ -1,28 +1,59 @@
 package selfhelp.viewpager;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 public class LoginActivity extends Activity {
 
-    Button btnLogin,btnSignup;
+    Button btnLogin, btnSignup;
 
     EditText txtMobileNo, txtPassword;
+    String authenticationcheck = "";
+
+    // Defined URL  where to send data
+
 
     // User Session Manager Class
     UserSessionManager session;
+
+    //constructor
+    public LoginActivity() {
+
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
 
         // User Session Manager
         session = new UserSessionManager(LoginActivity.this);
@@ -41,6 +72,31 @@ public class LoginActivity extends Activity {
 
         // User Signup button
         btnSignup = (Button) findViewById(R.id.btnSignup);
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+
+                    public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+                        //No need to implement.
+                    }
+
+                    public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+                        //No need to implement.
+                    }
+                }
+        };
+
+        // Install the all-trusting trust manager
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
         // Login button click event
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -52,48 +108,40 @@ public class LoginActivity extends Activity {
                 String username = txtMobileNo.getText().toString();
                 String password = txtPassword.getText().toString();
 
-                // Validate if username, password is filled
-                if(username.trim().length() > 0 && password.trim().length() > 0){
+
+               /* // Validate if username, password is filled
+                if(username.trim().length() > 0 && password.trim().length() > 0) {
 
                     // For testing puspose username, password is checked with static data
                     // username = admin
                     // password = admin
+*/
+                //TODO async task implementation
+                new getcanlogin().execute();
 
-                    //TODO async task implementation
-                   // new getcanlogin().execute();
 
+                if ((username.equals("9167034191") && password.equals("1234") || authenticationcheck.contains("P100"))) {
 
-                    if(username.equals("9167034191") && password.equals("1234")){
+                    // Creating user login session
+                    // Statically storing name="Android Example"
+                    // and email="androidexample84@gmail.com"
+                    session.createUserLoginSession(username, password);
 
-                        // Creating user login session
-                        // Statically storing name="Android Example"
-                        // and email="androidexample84@gmail.com"
-                        session.createUserLoginSession(username, password);
+                    // Starting MainActivity
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-                        // Starting MainActivity
-                        Intent i = new Intent(LoginActivity.this, MainActivity.class);
-                        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-                        // Add new Flag to start new Activity
-                        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        startActivity(i);
-                        finish();
-
-                    }else{
-
-                        // username / password doesn't match
-                        Toast.makeText(getApplicationContext(), "Mobile number/Password is incorrect", Toast.LENGTH_LONG).show();
-
-                    }
-                }else{
-
-                    // user didn't entered username or password
-                    Toast.makeText(getApplicationContext(), "Please enter Mobile number and Password", Toast.LENGTH_LONG).show();
+                    // Add new Flag to start new Activity
+                    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(i);
+                    finish();
 
                 }
-
             }
+
+
         });
+
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -106,8 +154,7 @@ public class LoginActivity extends Activity {
     }
 
     //TODO AsyncTask method completion
-   /* private class getcanlogin extends AsyncTask<String,String,String>
-    {
+    private class getcanlogin extends AsyncTask<String, String, String> {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -115,9 +162,18 @@ public class LoginActivity extends Activity {
 
         @Override
         protected String doInBackground(String... params) {
+            if (checkInternetConnection())
+                try {
+                    URL url;
+                    url = new URL("https://prms.ril.com/RBoxWS/RBoxWS.asmx/AuthenticateUser?");
+                    authenticationcheck = GetText(url);
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
 
-
-            return "xyz";
+            return authenticationcheck;
         }
 
         @Override
@@ -125,5 +181,113 @@ public class LoginActivity extends Activity {
 
 
         }
-    }*/
+    }
+
+    // Create GetText Metod
+    public String GetText(URL url) throws UnsupportedEncodingException {
+        String username = txtMobileNo.getText().toString();
+        String password = txtPassword.getText().toString();
+
+        // Create data variable for sent values to server
+        String concatunp = "in\\" + username + ":" + password;
+        String authstring = Base64.encodeToString(concatunp.getBytes(), Base64.URL_SAFE | Base64.NO_WRAP);
+
+        String text = "";
+        BufferedReader reader = null;
+
+        // Send data
+        try {
+
+            // Send POST data request
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestProperty("Authorization", "Basic " + authstring);
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+//            conn.setRequestProperty("userName", username);
+//            conn.setRequestProperty("password", password);
+
+//            userName=string&password=string
+            conn.connect();
+
+            String str = "userName=" + username + "&password=" + password;
+            byte[] outputInBytes = str.getBytes("UTF-8");
+            OutputStream os = conn.getOutputStream();
+            os.write(outputInBytes);
+            os.close();
+            // Get the server response
+
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+
+            // Read Server Response
+            while ((line = reader.readLine()) != null) {
+                // Append server response in string
+                sb.append(line + "\n");
+            }
+
+
+            text = sb.toString();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+
+                reader.close();
+            } catch (Exception ex) {
+            }
+        }
+
+        // Show response on activity
+        //////////// Toast.makeText(this,text,Toast.LENGTH_SHORT);
+
+        return text;
+    }
+
+    private boolean checkInternetConnection() {
+        // get Connectivity Manager object to check connection
+        ConnectivityManager connec = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Check for network connections
+        if (connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.CONNECTED ||
+                connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.CONNECTING ||
+                connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.CONNECTING ||
+                connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.CONNECTED) {
+            //////////////////////////   Toast.makeText(this, " Connected to internet", Toast.LENGTH_LONG).show();
+            return true;
+        } else if (
+                connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.DISCONNECTED ||
+                        connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.DISCONNECTED) {
+            //////////////////  Toast.makeText(this, " Not Connected to internet", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return false;
+    }
+
+  /*  public boolean isConnectedToServer(URL url, int timeout,String authstring) {
+        try {
+
+            url = new URL("https://prms.ril.com/RBoxWS/RBoxWS.asmx/AuthenticateUser?");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestProperty ("Authorization", "Basic " + authstring);
+            conn.setRequestMethod("POST");
+           // conn.setRequestProperty("username",username );
+           // conn.setRequestProperty("password",password );
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.setConnectTimeout(timeout);
+            conn.connect();
+             return true;
+
+        } catch (Exception e) {
+            //////////////////////////   Toast.makeText(this,"not connected to server",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+*/
+
+
 }
